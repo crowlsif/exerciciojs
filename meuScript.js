7616b08a6b409c74587001b8699d50f06f5002b1
Eric Antunes <eric.antunes.silva@gmail.com>
2020-06-17 22:00:46 -0300

    primeiro commit

---
+++ meuScript.js
@@ -0,0 +1,8 @@
+let primeironum = parseInt(window.prompt ("Insira o Primeiro Número"));
+let segundonum = parseInt(window.prompt ("Insira o Segundo Número"));
+
+if (primeironum > segundonum) {
+    alert(primeironum + " é o maior")
+} else {
+    alert(segundonum + " é o maior")
+}
